f8488f00ae84957929d7e4add9669f9cf3694047
Masi_Sediqi <154883375+Masi-Sediqi@users.noreply.github.com>
2025-11-09 23:58:03 +0430

    Switch to SQLite and update hero images
    Changed the database backend from PostgreSQL to SQLite for local development. Updated hero section CSS to use new 'sora' images and added four new PNG files for hero slides.

--- core/settings.py
+++ core/settings.py
@@ -98,17 +98,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ayan_shop',        # database name
+#         'USER': 'postgres',         # your PostgreSQL username
+#         'PASSWORD': 'your_password',# your PostgreSQL password
+#         'HOST': 'localhost',        # or your database server IP
+#         'PORT': '5432',             # default PostgreSQL port
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ayan_shop',        # database name
-        'USER': 'postgres',         # your PostgreSQL username
-        'PASSWORD': 'your_password',# your PostgreSQL password
-        'HOST': 'localhost',        # or your database server IP
-        'PORT': '5432',             # default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
